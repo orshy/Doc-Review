ddfabed93314f2af05d00bf0c36dbd1b18fae6d9
Philip Orchard <philiporchard@gmail.com>
2016-04-02 15:37:23 -0700

    add new form for rendering

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,4 +1,19 @@
 class DocsController < ApplicationController
+
   def index
   end
+
+  def new
+    @doc = Doc.new
+  end
+
+  def create
+    @doc = Doc.new(doc_params)
+  end
+
+  private
+
+    def doc_params
+      params.require(:doc).permit(:title, :description, :director)
+    end
 end
