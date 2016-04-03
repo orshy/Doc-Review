babbd9dac6744be55b75b150033c68ddc418fd10
Philip Orchard <philiporchard@gmail.com>
2016-04-03 13:04:26 -0700

    add raty.js for rating docs

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,51 @@
 class ReviewsController < ApplicationController
+  before_action :find_doc
+  before_action :find_review, only: [:edit, :update, :destroy]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.doc_id = @doc.id
+    @review.user_id = current_user.id
+
+    if @review.save
+      redirect_to doc_path(@doc)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to doc_path(@doc)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to doc_path(@doc)
+  end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+
+    def find_doc
+      @doc = Doc.find(params[:doc_id])
+    end
+
+    def find_review
+      @review = Review.find(params[:id])
+    end
+
 end
